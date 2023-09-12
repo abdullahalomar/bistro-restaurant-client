fa3e56b530afde0257c7b792a2d29c7fc77759c5
abdullah Al Omar <abdullahalomar048@gmail.com>
2023-09-12 23:44:23 +0600

    firebase auth and set provider

---
+++ src/providers/AuthProvider.jsx
@@ -0,0 +1,24 @@
+import { createContext, useState } from "react";
+import { getAuth } from "firebase/auth";
+import { app } from "../firebaase/firebase.config";
+
+export const AuthContext = createContext(null);
+
+const AuthProvider = ({children}) => {
+    const [user, setUser] = useState(null);
+    const [loading, setLoading] = useState(true);
+    const auth = getAuth(app);
+
+    const authInfo = {
+        user,
+        loading,
+    }
+
+    return (
+        <AuthContext.Provider value={authInfo}>
+            {children}
+        </AuthContext.Provider>
+    );
+};
+
+export default AuthProvider;
