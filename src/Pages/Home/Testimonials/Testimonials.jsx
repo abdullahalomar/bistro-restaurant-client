d72706886dbf4d825162788d56af2c98c745f2b0
abdullah Al Omar <abdullahalomar048@gmail.com>
2023-09-12 18:28:14 +0600

    login page created and added captcha

--- src/Pages/Home/Testimonials/Testimonials.jsx
+++ src/Pages/Home/Testimonials/Testimonials.jsx
@@ -17,7 +17,7 @@ const Testimonials = () => {
     const [reviews, setReviews] = useState([]);
 
     useEffect( () => {
-        fetch('reviews.json')
+        fetch('http://localhost:5000/reviews')
         .then(Response=> Response.json())
         .then(data => setReviews(data))
     },[])
