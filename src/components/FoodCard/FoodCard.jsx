90a7385b235b00d38cda9cd9151c1ded4872ecaa
abdullah Al Omar <abdullahalomar048@gmail.com>
2023-09-15 02:30:19 +0600

    just create a hook this name usecart

--- src/components/FoodCard/FoodCard.jsx
+++ src/components/FoodCard/FoodCard.jsx
@@ -1,24 +1,32 @@
 import { useContext } from "react";
 import { AuthContext } from '../../providers/AuthProvider';
 import Swal from "sweetalert2";
-import { useNavigate } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 
 const FoodCard = ({ item }) => {
-  const { name, recipe, image, price } = item;
+  const { name, recipe, image, price, _id } = item;
   const {user} = useContext(AuthContext);
   const navigate = useNavigate();
+  const location = useLocation();
 
   const handleAddToCart = item => {
       console.log(item);
-      if (user) {
-        fetch('http://localhost:5000/carts')
+      if (user && user.email) {
+        const cartItem = {menuItemId: _id, name, image, price, email: user.email}
+        fetch('http://localhost:5000/carts', {
+              method: 'POST',
+              headers:{
+                'content-type': 'application/json'
+              },
+              body: JSON.stringify(cartItem)
+        })
         .then(res => res.json())
         .then(data => {
           if (data.insertedId) {
             Swal.fire({
               position: 'top-end',
               icon: 'success',
-              title: 'Your work has been saved',
+              title: 'Food added on the cart.',
               showConfirmButton: false,
               timer: 1500
             })
@@ -35,7 +43,7 @@ const FoodCard = ({ item }) => {
           confirmButtonText: 'Login Now'
         }).then((result) => {
           if (result.isConfirmed) {
-            navigate('/login');
+            navigate('/login', {state: {from: location}});
           }
         })
       }
