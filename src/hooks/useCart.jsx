d590d7f40820cbd11892329febe510805fb726dc
abdullah Al Omar <abdullahalomar048@gmail.com>
2023-09-19 21:26:51 +0600

    get unautorized message use jwt

--- src/hooks/useCart.jsx
+++ src/hooks/useCart.jsx
@@ -3,15 +3,18 @@ import { useContext } from 'react';
 import { AuthContext } from '../providers/AuthProvider';
 const useCart = () => {
     const {user} = useContext(AuthContext);
+    const token = localStorage.getItem('access-token');
 
     const { refetch, data: cart = [] } = useQuery({
         queryKey: ['carts', user?.email],
         queryFn: async () => {
-            const response = await fetch(`http://localhost:5000/carts?email=${user?.email}`);
-            // if (!response.ok) {
-            //   throw new Error('Network response was not ok')
-            // }
-            return response.json()
+            const response = await fetch(`http://localhost:5000/carts?email=${user?.email}`, 
+            {
+                headers: {
+                    authorization: `bearer ${token}`
+                }
+            });
+            return response.json();
           },
       })
       return [cart, refetch]
