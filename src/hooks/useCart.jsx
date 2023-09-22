814ab3f20c70524703b7d631f72b9439b6a2159a
abdullah Al Omar <abdullahalomar048@gmail.com>
2023-09-23 02:45:22 +0600

    error

--- src/hooks/useCart.jsx
+++ src/hooks/useCart.jsx
@@ -7,9 +7,10 @@ const useCart = () => {
     const [axiosSecure] = useAxiosSecure();
     const { refetch, data: cart = [] } = useQuery({
         queryKey: ['carts', user?.email],
-        enabled: !loading,
+        // enabled: !loading,
+        enabled: !loading && !!user?.email,
         queryFn: async () => {
-            const res = await axiosSecure(`/carts?email=${user?.email}`);
+            const res = await axiosSecure(`/carts?email=${user?.email}`)
             console.log('res from axios', res)
             return res.data;
         },
@@ -19,29 +20,6 @@ const useCart = () => {
 
 }
 export default useCart;
-
-
-
-// import { useQuery } from '@tanstack/react-query'
-// import useAuth from './useAuth';
-// const useCart = () => {
-//      const { user, loading } = useAuth();
-//     // const token = localStorage.getItem('access-token');
-
-//     const { refetch, data: cart = [] } = useQuery({
-//         queryKey: ['carts', user?.email],
-//         enabled: !loading,
-//         queryFn: async () => {
-//             const res = await fetch(`http://localhost:5000/carts?email=${user?.email}`)
-//             console.log('res from axios', res)
-//             return res.json();
-//         },
-//     })
-
-//     return [cart, refetch]
-
-// }
-// export default useCart;
 
 
 
--- src/providers/AuthProvider.jsx
+++ src/providers/AuthProvider.jsx
@@ -48,16 +48,13 @@ const AuthProvider = ({children}) => {
                 .then(data => {
                     // console.log(data.data.token);
                     localStorage.setItem('access-token', data.data.token)
-                    
-                })
+                     
+                });
             }
             else{
-                
                 localStorage.removeItem('access-token')
             }
-            setLoading(false);        
-            
-
+            setLoading(false); 
         });
         return () => {
             return unsubscribe();
