f33ce33aa14090965d723216c4481709cf9818ec
abdullah Al Omar <abdullahalomar048@gmail.com>
2023-10-05 22:45:53 +0600

    get admin states data

--- src/Pages/Dashboard/AdminHome/AdminHome.jsx
+++ src/Pages/Dashboard/AdminHome/AdminHome.jsx
@@ -1,11 +1,26 @@
+import { useQuery } from "@tanstack/react-query";
 import useAuth from "../../../hooks/useAuth";
+import useAxiosSecure from "../../../hooks/useAxiosSecure";
+
 
 
 const AdminHome = () => {
-    const {user} = useAuth()
+    const {user} = useAuth();
+    const [axiosSecure] = useAxiosSecure()
+
+    const { data: stats = {} } = useQuery({
+        queryKey: ['admin-stats'],
+        queryFn: async () => {
+            const res = await axiosSecure('/admin-stats');
+            return res.data;
+        }
+    });
+
     return (
         <div className="w-full">
-            <p className="text-2xl font bold">Welcome Back {user.displayName}</p>
+            <div className="mx-10">
+            <p className="text-2xl font bold"><span className="text-blue-500 font-bold">Hi</span> Welcome Back {user.displayName}</p>
+            </div>
         </div>
     );
 };
